0b5d0d84bd25cda72070a7916d6911ecb4fcec35
William Balbino <williambalbino14@hotmail.com>
2017-08-19 23:26:17 -0300

    ad

---
+++ myApp/src/providers/movie/movie.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the MovieProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular DI.
+*/
+@Injectable()
+export class MovieProvider {
+
+  baseApiPath = "https://api.themoviedb.org/3";
+
+  constructor(public http: Http) {
+    console.log('Hello MovieProvider Provider');
+  }
+
+  getLatestMovies(){
+    return this.http.get(this.baseApiPath + "/movie/popular?api_key=05edef0ce288a710a7751acf9da1dc30")
+  }
+
+}
